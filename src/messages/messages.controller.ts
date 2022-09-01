7be2f42a941cafbc8850964fab1806f4a62de760
Erik S. Carlsten <ecarlste@gmail.com>
2022-09-01 11:42:48 -0600

    Added basic controller functionality

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Post()
+  createMessage(@Body() body) {
+    console.log(body);
+  }
+
+  @Get()
+  listMessages() {
+    console.log('list messages');
+  }
+
+  @Get(':id')
+  getMessage(@Param('id') id: string) {
+    console.log(id);
+  }
+}
