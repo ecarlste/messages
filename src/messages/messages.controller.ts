4e171e75685988b7d37c2f69b0cbb4e2f2124364
Erik S. Carlsten <ecarlste@gmail.com>
2022-09-01 13:23:40 -0600

    Added use of messages service without  DI

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,8 +1,15 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from 'dto/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
+
   @Post()
   createMessage(@Body() createMessageDto: CreateMessageDto) {
     console.log(createMessageDto);
